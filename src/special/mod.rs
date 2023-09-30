27622c7dee6668ffc67e85f5a387114a8beb5179
Dizzyi <deeralan827@gmail.com>
2023-09-30 18:34:07 +0800

    optimization generalization and scalar root finding

--- src/special/mod.rs
+++ src/special/mod.rs
@@ -7,10 +7,7 @@ pub use trig::*;
 
 pub fn i0(x: Array1<f64>) -> Result<Array1<Complex<f64>>, i32> {
     // TODO implement this using cephes
-    todo!();
-    /*
     x.mapv(|v| complex_bessel_rs::bessel_i::bessel_i(0.0, v.into()))
     .into_iter()
     .collect()
-    */
 }
